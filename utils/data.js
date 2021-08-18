b9313d086f8ba00e53ae5154c5020f0e5bb01cb0
unknown <buzesvictor@gmail.com>
2021-08-18 09:17:25 +0300

    firts commit

---
+++ utils/data.js
@@ -0,0 +1,87 @@
+const data = {
+    products: [
+        {
+            name: 'Free Shirts',
+            slug: 'free-shirts',
+            category: 'Shirts',
+            image: '/images/shirt1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shit',
+
+        },
+        {
+            name: 'Fit Shirts',
+            slug: 'fit-shirts',
+            category: 'Shirts',
+            image: '/images/shirt2.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shit',
+
+        },
+        {
+            name: 'Slim Shirts',
+            slug: 'slim-shirts',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 90,
+            brand: 'Raymond',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shit',
+
+        },
+        {
+            name: 'Golf Pants',
+            slug: 'golf-pants',
+            category: 'Pants',
+            image: '/images/pants1.jpg',
+            price: 160,
+            brand: 'Oliver',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'Smart Loooking Pants',
+
+        },
+        {
+            name: 'Fit Pants',
+            slug: 'fit-pants',
+            category: 'Pants',
+            image: '/images/pants2.jpg',
+            price: 100,
+            brand: 'Zara',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular pants',
+
+        },
+        {
+            name: 'Classic Pants',
+            slug: 'classic-pants',
+            category: 'Pants',
+            image: '/images/pants3.jpg',
+            price: 79,
+            brand: 'Casely',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular pants',
+
+        },
+
+       
+
+    ],
+};
+
+export default  data;
